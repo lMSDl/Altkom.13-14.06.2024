702edd798e3f42c5ab194165fcbf469788a14eef
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-13 14:27:36 +0100

    Main test

---
+++ ConsoleApp.Test.xUnit/ProgramTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Test.xUnit
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void Main_ConsoleOutput_HelloWorld()
+        {
+            //Arrange
+            var main = typeof(Program).Assembly.EntryPoint;
+            TextWriter consoleWriter;
+            using StringWriter writer = new StringWriter();
+            SwitchConsoleOut(writer, out consoleWriter);
+
+            //Act
+            main.Invoke(null, new object[] { Array.Empty<string>() });
+
+            //Assert
+            SwitchConsoleOut(consoleWriter, out _);
+            Assert.Equal("Hello, World!\r\n", writer.ToString());
+        }
+
+        private static void SwitchConsoleOut(TextWriter @int, out TextWriter @out)
+        {
+            @out = Console.Out;
+            Console.SetOut(@int);
+        }
+    }
+}
